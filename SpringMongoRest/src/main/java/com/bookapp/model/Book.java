82452ba30b3becad7d199b5868e1e6424d2d8c5b
99002785 <72495720+99002785@users.noreply.github.com>
2020-10-23 17:47:36 +0530

    Add files via upload

---
+++ SpringMongoRest/src/main/java/com/bookapp/model/Book.java
@@ -0,0 +1,85 @@
+package com.bookapp.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "book")
+@TypeAlias("Book")
+
+public class Book {
+	
+	@Id
+	Integer bookId;
+	String title;
+	String author;
+	String category;
+	Double price;
+	
+	
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+	public Book(Integer bookId, String title, String author, Double price, String category) {
+		super();
+		this.bookId = bookId;
+		this.title = title;
+		this.author = author;
+		this.category = category;
+		this.price = price;
+	}
+
+
+	public Integer getBookId() {
+		return bookId;
+	}
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Book [bookId=" + bookId + ", title=" + title + ", author=" + author + ", category=" + category
+				+ ", price=" + price + "]";
+	}
+	
+	
+	
+
+}
